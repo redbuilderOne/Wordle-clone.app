5b4163c84fcad24842aa978152f40eef667816e5
redbuilderOne <forsaken2492@yandex.ru>
2022-11-29 19:50:56 +0300

    collection view is added

---
+++ Wordle-Clone.App/Core/KeyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  KeyCollectionViewCell.swift
+//  Wordle-Clone.App
+//
+//  Created by Дмитрий Скворцов on 29.11.2022.
+//
+
+import UIKit
+
+class KeyCollectionViewCell: UICollectionViewCell {
+    static let identifier = "KeyCollectionViewCell"
+    
+}
--- Wordle-Clone.App/Core/KeyboardViewController.swift
+++ Wordle-Clone.App/Core/KeyboardViewController.swift
@@ -9,12 +9,75 @@ import UIKit
 
 class KeyboardViewController: UIViewController {
 
+    let letters = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+    var keys: [[Character]] = []
+
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .green
+        collectionView.register(KeyCollectionViewCell.self, forCellWithReuseIdentifier: KeyCollectionViewCell.identifier)
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(collectionView)
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
         view.backgroundColor = .red
+        activateConstraints()
+        appendCharacters()
+    }
 
+    private func appendCharacters() {
+        for row in letters {
+            let characters = Array(row)
+            keys.append(characters)
+        }
+    }
 
+    private func activateConstraints() {
+        let collectionViewConstraints = [
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+
+        NSLayoutConstraint.activate(collectionViewConstraints)
+    }
+
+}
+
+extension KeyboardViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
     }
 
 
